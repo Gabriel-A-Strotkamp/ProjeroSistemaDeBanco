f2b58506e01b4c274e69b281498aa0c11752237d
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2025-06-27 11:48:32 -0300

    Add files via upload

--- target/generated-sources/annotations/br/edu/ifsul/lpoo/model/Agencias_.java
+++ target/generated-sources/annotations/br/edu/ifsul/lpoo/model/Agencias_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-23T07:12:12", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-27T11:33:51", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Agencias.class)
 public class Agencias_ { 
 
--- target/generated-sources/annotations/br/edu/ifsul/lpoo/model/Pessoas_.java
+++ target/generated-sources/annotations/br/edu/ifsul/lpoo/model/Pessoas_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-23T07:12:12", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-27T11:33:51", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Pessoas.class)
 public abstract class Pessoas_ { 
 
